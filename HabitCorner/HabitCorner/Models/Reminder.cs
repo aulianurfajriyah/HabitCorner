1db01bd111a422952e292afea842581c79d9ce5b
adhyaksazhalifunnas <adhyaksazhalifunnas@gmail.com>
2022-09-20 23:07:23 +0700

    Started Visual Studio project --> add class (to do: implement method)

---
+++ HabitCorner/HabitCorner/Models/Reminder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitCorner_Class
+{
+    class Reminder
+    {
+        //Instance variable
+        private int reminderID;
+        private string reminderTime;
+        private int reminderSetting;
+
+        public int ReminderID { get => reminderID; set => reminderID = value; }
+        public string ReminderTime { get => reminderTime; set => reminderTime = value; }
+        public int ReminderSetting { get => reminderSetting; set => reminderSetting = value; }
+
+        public static void settingReminder()
+        {
+            //Implement reminder setting here
+        }
+    }
+}
